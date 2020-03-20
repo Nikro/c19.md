485fc634124bf4d04ae8add084640eb8bbd40ab6
Sergiu Nagailic (Nikro) <nikro.md@gmail.com>
2020-03-20 01:50:25 +0100

    Debugging.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,22 @@
  */
 const path = require(`path`)
 
+function flattenMessages(nestedMessages, prefix = "") {
+  return Object.keys(nestedMessages).reduce((messages, key) => {
+    let value = nestedMessages[key]
+    let prefixedKey = prefix ? `${prefix}.${key}` : key
+
+    if (typeof value === "string") {
+      messages[prefixedKey] = value
+    } else {
+      Object.assign(messages, flattenMessages(value, prefixedKey))
+    }
+
+    return messages
+  }, {})
+}
+
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
   const simplePageTemplate = path.resolve(`src/templates/simple-page.js`)
@@ -24,6 +40,24 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       }
     }
   `)
+  const getMessages = (path, language) => {
+    try {
+      // TODO load yaml here
+      const messages = require(`${path}/${language}.json`)
+
+      return flattenMessages(messages)
+    } catch (error) {
+      if (error.code === "MODULE_NOT_FOUND") {
+        process.env.NODE_ENV !== "test" &&
+        console.error(
+          `[gatsby-plugin-intl] couldn't find file "${path}/${language}.json"`
+        )
+      }
+
+      throw error
+    }
+  }
+
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
@@ -34,38 +68,18 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       path: node.frontmatter.path,
       component: simplePageTemplate,
       context: {
-        type: "markdown",
-        locale: node.frontmatter.locale
+        language: node.frontmatter.locale,
+        intl: {
+          language: node.frontmatter.locale,
+          languages: [node.frontmatter.locale],
+          messages: getMessages(`${__dirname}/src/intl`, node.frontmatter.locale),
+          routed: false,
+          originalPath: node.frontmatter.path,
+          redirect: false,
+          defaultLanguage: 'ro',
+        },
       }
     })
   })
 }
 
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage, deletePage } = actions
-
-
-  if (typeof page.context.type === "string" && page.context.type === "markdown" ) {
-
-    console.log('-------------')
-
-    // languages = ['ro', 'ru', 'en']
-    // languages.forEach(language => {
-      if (page.context.language !== page.context.locale) {
-        deletePage(page)
-
-        console.log('-------- DELETED -----')
-        console.log(page)
-      }
-      else {
-        console.log('-------- REM -----')
-        console.log(page)
-      }
-      if (page.path.includes('/ro/')) {
-        deletePage(page)
-        page.path = page.context.intl.originalPath
-        createPage(page)
-      }
-    // })
-  }
-}
